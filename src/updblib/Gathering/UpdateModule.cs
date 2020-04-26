ad3f46017c74d1cf2f5baf42136b68a83f3e69e9
Takeshi Katano <takatano@outlook.com>
2020-04-27 00:32:46 +0900

    Add UpdateModule class

---
+++ src/updblib/Gathering/UpdateModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace updblib.Gathering
+{
+    public class UpdateModule
+    {
+        public static UpdateModule RetrieveData(string updateModuleFilePath)
+        {
+            return new UpdateModule();
+        }
+    }
+}
